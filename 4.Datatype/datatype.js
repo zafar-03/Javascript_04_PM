7e9cd9d569e69890bc4ee8401a5473b1267a56c0
zafar-03 <zfnanshabhai@gmail.com>
2026-06-05 16:49:43 +0530

    Variable and Datatype

---
+++ 4.Datatype/datatype.js
@@ -0,0 +1,57 @@
+console.log("Here");
+
+// tyopeof Operator : 
+    // variable/data -> type return
+
+    // syntax : 
+        // 1.typeof(data)
+        // 2.typeof data
+
+
+// 1. number : int,float
+
+console.log(12);
+console.log(3.14);
+console.log(typeof(12));
+console.log(typeof(3.14));
+
+
+// 2. string : string,char
+console.log('Sahil');
+console.log("Raj");
+console.log(typeof 'A');
+console.log(typeof "Raj");
+
+
+// 3. Boolean : true,false
+
+console.log(true);
+console.log(typeof true);
+
+
+// 4. Object : object,array,null
+console.log(null);
+console.log([11,12,13]);
+console.log({"fname" : "Raj"});
+console.log(typeof null);
+console.log(typeof [11,12,13]);
+console.log(typeof {"fname" : "Raj"});
+
+
+// console.log(Array.isArray({"fname" : "Raj"}));
+
+
+// 5. BigInt : >number
+console.log(12345678901234567890n);
+console.log(typeof 12345678901234567890n);
+
+
+// 6. undefined : undefined
+console.log( undefined);
+console.log(typeof undefined);
+
+// 7. Function : 
+console.log(typeof console.log);
+
+
+// 8. Symbol
---
+++ 5.Variable/variable.js
@@ -0,0 +1,40 @@
+console.log("what is variables understanding data types declaring and initialization variables");
+
+// in C : 
+// int num;
+// char ch;
+// float arr[];
+
+
+// in JS : 
+// let, var , const
+
+// var firstname = "Rajesh";
+// let firstname = "Rajan";
+// const firstname = 123;
+
+// console.log(firstname);
+
+
+// 1. var
+var a1 = 11;
+console.log(a1);
+// a1 = 100;
+var a1 = 1000;
+console.log(a1);
+
+
+// 2. let
+let b1 = 12;
+console.log(b1);
+// let b1 = 2000;
+console.log(b1);
+
+
+
+// 3. const
+const c1 = 13;
+console.log(c1);
+// const c1 = 3000;
+console.log(c1);
+
