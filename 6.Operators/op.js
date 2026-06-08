cb17651ce6da22543d7db8bba2f9a8e9b80c49e4
zafar-03 <rwr2.mohammadzafar.as@gmail.com>
2026-06-08 16:59:57 +0530

    Operators

---
+++ 6.Operators/op.js
@@ -0,0 +1,57 @@
+// console.log("+ - * / %   ++ --");
+
+
+// // console.log(12 % 5); // 2
+
+// console.log("12"+1);// concatination // 121
+// console.log("12"-1);//11
+
+// console.log("12"-true);
+// console.log(true + 12); 
+
+// console.log("= += -= *= /= %=");
+
+// console.log("== != < <= > >=  === !==");
+// console.log(11 != 12);
+// console.log(11 == "11");
+// console.log(11 === "11");
+
+// console.log("&& || !");
+
+
+// AND(&&) : return true/1 if All Operations are true/1 else return false/0.
+// OR(||) : return False/0 if All Operations are fale/0 else return true/1.
+// NOT(!) : return True/1 if Operations are False else return False.
+
+
+// console.log(11<12  &&  11!=12);
+// console.log(11>12  &&  11!=12);
+// console.log(11<12  ||  11!=12);
+// console.log(11>12  ||  11==12);
+
+// console.log(!(11>12  ||  11==12));
+
+
+// truthy Value: 
+// true ,1 , numbers,[],{}," "
+
+// falsy value:
+// false , 0 , "" , null , undefined
+
+// console.log(Boolean(undefined));
+
+
+// console.log(null && 13);
+
+// console.log(0 || 13);
+// console.log(11 || null);
+
+
+
+// Boolean
+// Number 
+// String
+
+// console.log(Number("Raj"),typeof Number("Raj"));
+
+document.querySelector('h1').innerHTML = String(true);
