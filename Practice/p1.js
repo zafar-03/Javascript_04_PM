76ba7f5f508907744cabbe5ab2547b6c53977ab5
zafar-03 <rwr2.mohammadzafar.as@gmail.com>
2026-06-09 17:05:33 +0530

    Practice

---
+++ Practice/p1.js
@@ -0,0 +1,29 @@
+// Ternary Operator :-
+
+//     (codition) ? st1 : st2;  
+
+// var num1 = -10;
+// (num1>0) ? console.log("+ve") : console.log("-ve");
+
+
+var age = 15;
+
+var product = (age<=10)? "Milk" : "Tea";
+
+console.log(product);
+
+
+// leap : 
+// divide by 4 :
+//         divide by 100 :
+    //          divide by 400: leap              
+    //          else : not a leap year
+    //     not divide by 100 : 
+                //leap
+
+// not divide by 4 : no leap year 
+    
+
+// prompt
+// confirm
+// alert 
