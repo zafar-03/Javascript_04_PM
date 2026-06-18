e823cb2f9d5d2308dffd41f8b826073d18d8e431
zafar-03 <zfnanshbahi@gmail.com>
2026-06-18 16:48:12 +0530

    Array String

---
+++ 10.String/string.js
@@ -0,0 +1,54 @@
+// What is a String : 
+//  It's Collection of Char.
+//  Index : 0
+
+// Syntax : ""    /  ''      /   `` 
+// console.log(typeof "Javascript");    
+// console.log(typeof 'Javascript');    
+// console.log(typeof `Javascript`);    
+
+
+// console.log("This a First Line.\nThis a Second.");
+// console.log('This a First Line.\n\tThis a Second.');
+
+// // Multiline String  : ``
+// console.log(`
+//     This a First Line.
+//     This a Second.
+//     `);
+
+
+// var fname = "Raj";
+// var age = 12;
+
+// console.log("Your Name is "+fname+".Your age is"+age+".");
+// console.log('Your Name is',fname,'.Your age is',age,'.');
+
+
+
+// String Formating : Litral String : 
+
+// console.log(`Your name is ${fname}.Your age is ${age}.`);
+
+// console.log('Your name is ${fname}.Your age is ${age}.');
+// console.log("Your name is ${fname}.Your age is ${age}.");
+
+
+
+var str = "This a Javascript";
+// console.log(str[0]);
+
+// property : length
+// console.log(str.length);
+
+
+// Methods :
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.includes("z"));
+// console.log(str.split(" "))
+
+
+
+// splice   : method
+// console.log(str.split());
---
+++ 11.Array/array1.js
@@ -0,0 +1,53 @@
+// What is an Array : 
+// It's a Collection Data which is Same/different Datatype.
+// index : 0
+// syntax :  []
+
+// empty Array : 
+// const arr = [];
+// const arr = [1,2,3,4,5];
+// const arr = ["Zafar","Raj"];
+// const arr = [1,2,3,"Zafar",true,null,undefined];
+
+
+// console.log(arr);
+
+// property : 
+// console.log(arr.length);
+
+
+// var students = ["Krunal","Shilesh"];
+// console.log(students);
+
+// Add 
+// 1 ending : push
+// students.push("Rajesh");
+// console.log(students);
+
+// students.push("Rajesh","Rahul");
+// console.log(students);
+
+// 2 starting : unshift
+// students.unshift("Rajveer");
+// console.log(students);
+// students.unshift("Rajveer","Sahil");
+// console.log(students);
+
+// remove 
+// 1. starting 
+// students.shift();
+// 2. ending 
+// students.pop();
+// console.log(students);
+
+
+var students = ["Krunal","Shilesh","Rahul","Sahil"];
+// console.log(students.splice());
+
+// console.log(students.splice(0));
+// console.log(students.splice(2));
+
+
+// console.log(students.splice(2,2))
+// console.log(students);
+
